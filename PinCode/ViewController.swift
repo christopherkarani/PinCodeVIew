5d6a687d518b763c48eb3014200e3610125b1166
Chris Karani <christopher.karani@incentro.com>
2018-09-29 16:41:32 +0300

    Initial Commit

---
+++ PinCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinCode
+//
+//  Created by Chris Karani on 29/09/2018.
+//  Copyright © 2018 Chris Karani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
